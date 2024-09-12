48f25974285e0f6227799c41fcc330e0bfb46c34
Mondli Mkhanyiswa <mondlimkha.mm@gmail.com>
2024-09-12 21:46:37 +0200

    added admin & charts pages

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -112,32 +112,3 @@ const menuItems = [
     ],
   },
 ];
-
-import Link from "next/link";
-import Image from "next/image";
-
-const Menu = () => {
-  return (
-    <div className="mt-4 text-sm">
-      {menuItems.map((i) => (
-        <div className="flex flex-col gap-2" key={i.title}>
-          <span className="hidden lg:block text-gray-400 font-light my-4">
-            {i.title}
-          </span>
-          {i.items.map((items) => (
-            <Link
-              href={items.href}
-              key={items.label}
-              className="flex items-center justify-center lg:justify-start gap-4 text-grap-500 py-2"
-            >
-              <Image src={items.icon} alt="" width={20} height={20} />
-              <span className="hidden lg:block">{items.label}</span>
-            </Link>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Menu;
