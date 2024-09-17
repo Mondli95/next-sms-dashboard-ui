3268c2c383c38346e9c8926d9860b72ea4e1c7fa
Mondli Mkhanyiswa <mondlimkha.mm@gmail.com>
2024-09-17 21:05:41 +0200

    finished teachers single page

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,4 +1,8 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import PerformanceChart from "@/components/PerformanceChart";
 import Image from "next/image";
+import Link from "next/link";
 
 const SingleTeacherPage = () => {
   return (
@@ -16,20 +20,114 @@ const SingleTeacherPage = () => {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
-              <h1>Madge Thornton</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-                facere dolor illum sequi, itaque provident distinctio suscipit
-                adipisci placeat deleniti odio nesciunt! Harum mollitia rerum
-                voluptate consectetur perferendis nam iste!
+              <h1 className="text-xl font-semibold">Madge Thornton</h1>
+              <p className="text-xs text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit
               </p>
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/blood.png" alt="" width={14} height={14} />
+                  <span>A+</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/date.png" alt="" width={14} height={14} />
+                  <span>January 2025</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/mail.png" alt="" width={14} height={14} />
+                  <span>madgethornton@email.com</span>
+                </div>
+                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                  <Image src="/phone.png" alt="" width={14} height={14} />
+                  <span>078 0236 0256</span>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="flex"></div>
+          <div className="flex-1 flex justify-between gap-4 flex-wrap">
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleAttendance.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <div className="text-xl font-semibold">90%</div>
+                <span className="text-sm text-gray-400">Attendance</span>
+              </div>
+            </div>
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleBranch.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <div className="text-xl font-semibold">3</div>
+                <span className="text-sm text-gray-400">Branches</span>
+              </div>
+            </div>
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleLesson.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <div className="text-xl font-semibold">99</div>
+                <span className="text-sm text-gray-400">Lessons</span>
+              </div>
+            </div>
+            <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+              <Image
+                src="/singleClass.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <div className="text-xl font-semibold">6</div>
+                <span className="text-sm text-gray-400">Classes</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="">Teachers Schedule</div>
+        <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
+          <h1 className="text-xl font-semibold">Teacher&apos;s Schedule</h1>
+          <BigCalendar />
+        </div>
       </div>
-      <div className="w-full xl:w-1/3">right</div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-skyLight" href="/">
+              Teacher&apos;s Students
+            </Link>
+            <Link className="p-3 rounded-md bg-purpleLight" href="/">
+              Teacher&apos;s Lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-yellowLight" href="/">
+              Teacher&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">
+              Teacher&apos;s Assignments
+            </Link>
+            <Link className="p-3 rounded-md bg-skyLight" href="/">
+              Teacher&apos;s Classes
+            </Link>
+          </div>
+        </div>
+        <PerformanceChart />
+        <Announcements />
+      </div>
     </div>
   );
 };
